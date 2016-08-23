aa54f5169be175ced542bb3e804c838b095b3355
foxydot <catherine@madsciencedept.com>
2016-08-23 22:06:03 +0000

    initial commit

---
+++ assets/plugins/msd-custom-cpt/lib/inc/msd_book_cpt.php
@@ -0,0 +1,138 @@
+<?php 
+if (!class_exists('MSDChapterCPT')) {
+	class MSDChapterCPT {
+		//Properties
+		var $cpt = 'chapter';
+		//Methods
+	    /**
+	    * PHP 4 Compatible Constructor
+	    */
+		public function MSDChapterCPT(){$this->__construct();}
+	
+		/**
+		 * PHP 5 Constructor
+		 */
+		function __construct(){
+			global $current_screen;
+        	//"Constants" setup
+        	$this->plugin_url = plugin_dir_url('msd-custom-cpt/msd-custom-cpt.php');
+        	$this->plugin_path = plugin_dir_path('msd-custom-cpt/msd-custom-cpt.php');
+			//Actions
+            add_action( 'init', array(&$this,'register_tax_chapters') );
+            add_action( 'init', array(&$this,'register_cpt_chapter') );
+            
+			add_action('admin_head', array(&$this,'plugin_header'));
+			add_action('admin_print_scripts', array(&$this,'add_admin_scripts') );
+			add_action('admin_print_styles', array(&$this,'add_admin_styles') );
+			// important: note the priority of 99, the js needs to be placed after tinymce loads
+			add_action('admin_print_footer_scripts',array(&$this,'print_footer_scripts'),99);
+		}
+		
+		
+        public function register_tax_chapters() {
+        
+            $labels = array( 
+                'name' => _x( 'Chapters', 'chapters' ),
+                'singular_name' => _x( 'Chapter', 'chapters' ),
+                'search_items' => _x( 'Search chapters', 'chapters' ),
+                'popular_items' => _x( 'Popular chapters', 'chapters' ),
+                'all_items' => _x( 'All chapters', 'chapters' ),
+                'parent_item' => _x( 'Parent chapter', 'chapters' ),
+                'parent_item_colon' => _x( 'Parent chapter:', 'chapters' ),
+                'edit_item' => _x( 'Edit chapter', 'chapters' ),
+                'update_item' => _x( 'Update chapter', 'chapters' ),
+                'add_new_item' => _x( 'Add new chapter', 'chapters' ),
+                'new_item_name' => _x( 'New chapter name', 'chapters' ),
+                'separate_items_with_commas' => _x( 'Separate chapters with commas', 'chapters' ),
+                'add_or_remove_items' => _x( 'Add or remove chapters', 'chapters' ),
+                'choose_from_most_used' => _x( 'Choose from the most used chapters', 'chapters' ),
+                'menu_name' => _x( 'Chapters', 'chapters' ),
+            );
+        
+            $args = array( 
+                'labels' => $labels,
+                'public' => true,
+                'show_in_nav_menus' => true,
+                'show_ui' => true,
+                'show_tagcloud' => false,
+                'hierarchical' => true, //we want a "category" style taxonomy, but may have to restrict selection via a dropdown or something.
+        
+                'rewrite' => array('slug'=>'chapter','with_front'=>false),
+                'query_var' => true
+            );
+        
+            register_taxonomy( 'chapters', array($this->cpt), $args );
+        }
+		
+		function register_cpt_chapter() {
+		
+		    $labels = array( 
+		        'name' => _x( 'Chapters', 'chapter' ),
+		        'singular_name' => _x( 'Chapter', 'chapter' ),
+		        'add_new' => _x( 'Add New', 'chapter' ),
+		        'add_new_item' => _x( 'Add New Chapter', 'chapter' ),
+		        'edit_item' => _x( 'Edit Chapter', 'chapter' ),
+		        'new_item' => _x( 'New Chapter', 'chapter' ),
+		        'view_item' => _x( 'View Chapter', 'chapter' ),
+		        'search_items' => _x( 'Search Chapter', 'chapter' ),
+		        'not_found' => _x( 'No chapter found', 'chapter' ),
+		        'not_found_in_trash' => _x( 'No chapter found in Trash', 'chapter' ),
+		        'parent_item_colon' => _x( 'Parent Chapter:', 'chapter' ),
+		        'menu_name' => _x( 'Chapter', 'chapter' ),
+		    );
+		
+		    $args = array( 
+		        'labels' => $labels,
+		        'hierarchical' => false,
+		        'description' => 'Chapter',
+		        'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'revisions','genesis-cpt-archives-settings'),
+		        'taxonomies' => array('chapter'),
+		        'public' => true,
+		        'show_ui' => true,
+		        'show_in_menu' => true,
+		        'menu_position' => 20,
+		        
+		        'show_in_nav_menus' => true,
+		        'publicly_queryable' => true,
+		        'exclude_from_search' => true,
+		        'has_archive' => true,
+		        'query_var' => true,
+		        'can_export' => true,
+		        'rewrite' => array('slug'=>'book','with_front'=>false),
+		        'capability_type' => 'post'
+		    );
+		
+		    register_post_type( $this->cpt, $args );
+        
+		}
+		
+		function plugin_header() {
+			global $post_type;
+		}
+		 
+		function add_admin_scripts() {
+			global $current_screen;
+			if($current_screen->post_type == $this->cpt){
+			}
+		}
+
+        function add_admin_styles() {
+            global $current_screen;
+            if($current_screen->post_type == $this->cpt){
+            }
+        }   
+			
+		function print_footer_scripts()
+		{
+			global $current_screen;
+			if($current_screen->post_type == $this->cpt){
+				?><script type="text/javascript">
+                    jQuery(function($){
+                    });
+                 </script><?php
+			}
+		}
+		
+            
+  } //End Class
+} //End if class exists statement
---
+++ assets/plugins/msd-custom-cpt/lib/inc/msd_team_display.php
@@ -0,0 +1,373 @@
+<?php 
+if (!class_exists('MSDTeamDisplay')) {
+    class MSDTeamDisplay {
+        //Properties
+        var $cpt = 'team_member';
+        //Methods
+        /**
+        * PHP 4 Compatible Constructor
+        */
+        public function MSDTeamDisplay(){$this->__construct();}
+    
+        /**
+         * PHP 5 Constructor
+         */
+        function __construct(){
+            global $current_screen;
+            //"Constants" setup
+            $this->plugin_url = plugin_dir_url('msd-custom-cpt/msd-custom-cpt.php');
+            $this->plugin_path = plugin_dir_path('msd-custom-cpt/msd-custom-cpt.php');
+            //Actions
+                     
+            //Filters
+            add_filter( 'genesis_attr_headshot', array(&$this,'msdlab_headshot_context_filter' ));
+            add_shortcode('team-members',array(&$this,'team_members_shortcode_handler'));
+            add_shortcode('team_members',array(&$this,'team_members_shortcode_handler'));
+        }
+ 
+        function get_team_member_by_practice($practice_area) {
+           $args = array(
+            'posts_per_page'   => -1,
+            'orderby'          => 'title',
+            'order'            => 'ASC',
+            'practice_area'    => $practice_area,
+            'post_type'        => $this->cpt,
+            );
+            $posts = get_posts($args);
+            $i = 0;
+            foreach($posts AS $post){
+                $posts[$i]->lastname = get_post_meta($post->ID,'_team_member__team_member_last_name',TRUE);
+                $i++;
+            }
+            usort($posts,array(&$this,'sort_by_lastname'));
+            return $posts;
+        }  
+        
+        function get_all_team_members(){
+            $args = array(
+            'posts_per_page'   => -1,
+            'orderby'          => 'title',
+            'order'            => 'ASC',
+            'post_type'        => $this->cpt,
+            );
+            $posts = get_posts($args);
+            $i = 0;
+            foreach($posts AS $post){
+                $posts[$i]->lastname = get_post_meta($post->ID,'_team_member__team_last_name',TRUE);
+                $i++;
+            }
+            usort($posts,array(&$this,'sort_by_lastname'));
+            return $posts;
+        }     
+        
+        function team_display($team,$attr = array()){
+            global $post,$msd_custom,$contact_info_metabox,$jobtitle_metabox;
+            extract($attr);
+            $headshot = get_the_post_thumbnail($team->ID,'headshot-md');
+            $terms = wp_get_post_terms($team->ID,'practice_area');
+            $jobtitle_metabox->the_meta($team->ID);
+            $practice_areas = '';
+            if(count($terms)>0){
+                foreach($terms AS $term){
+                    $practice_areas[] = $term->slug;
+                }
+                
+                $practice_areas = implode(' ', $practice_areas);
+            }
+            $mini_bio = msdlab_excerpt($team->ID);
+            $team_contact_info = '';
+            $contact_info_metabox->the_meta($team->ID);
+            $contact_info_metabox->the_field('_team_phone');
+            if($contact_info_metabox->get_the_value() != ''){ 
+                $team_contact_info .= '<li class="phone"><i class="fa fa-phone"></i>'.msd_str_fmt($contact_info_metabox->get_the_value(),'phone').'</li>';
+            } 
+            
+            $contact_info_metabox->the_field('_team_mobile');
+            if($contact_info_metabox->get_the_value() != ''){
+                $team_contact_info .= '<li class="mobile"><i class="fa fa-mobile-phone"></i> '.msd_str_fmt($contact_info_metabox->get_the_value(),'phone').'</li>';
+            }
+            
+            $contact_info_metabox->the_field('_team_linked_in');
+            if($contact_info_metabox->get_the_value() != ''){
+                $team_contact_info .= '<li class="linkedin"><a href="'.$contact_info_metabox->get_the_value().'"><i class="fa fa-linkedin-square"></i> Connect</a></li>';
+            }
+            
+            $contact_info_metabox->the_field('_team_bio_sheet');
+            if($contact_info_metabox->get_the_value() != ''){
+                $team_contact_info .= '<li class="vcard"><a href="'.$contact_info_metabox->get_the_value().'"><i class="fa fa-download-alt"></i> Download Bio</a></li>';
+            }
+            
+            $contact_info_metabox->the_field('_team_email');
+            if($contact_info_metabox->get_the_value() != ''){
+                $team_contact_info .= '<li class="email"><i class="fa fa-envelope"></i> '.msd_str_fmt($contact_info_metabox->get_the_value(),'email').'</li>';
+            }
+            $teamstr = '
+            <a class="team-member '.$practice_areas.' '.$team->post_name.'" href="'.get_post_permalink($team->ID).'">
+                <div class="headshot">
+                    '.$headshot.'
+                </div>
+                <div class="info">
+                    <h4>'.$team->post_title.'</h4>
+                    <h5>'.$jobtitle_metabox->get_the_value('jobtitle').'</h5>
+                    ';
+            $teamstr .= '
+                </div>
+            </a>';
+            return $teamstr;
+    }   
+        function team_display_full($team,$attr = array()){
+            global $post,$msd_custom,$contact_info_metabox,$jobtitle_metabox;
+            extract($attr);
+            $headshot = get_the_post_thumbnail($team->ID,'headshot-md');
+            $terms = wp_get_post_terms($team->ID,'practice_area');
+            $jobtitle_metabox->the_meta($team->ID);
+            $practice_areas = '';
+            if(count($terms)>0){
+                foreach($terms AS $term){
+                    $practice_areas[] = $term->slug;
+                }
+                
+                $practice_areas = implode(' ', $practice_areas);
+            }
+            $mini_bio = msdlab_get_excerpt($team->ID);
+            $team_contact_info = '';
+            $contact_info_metabox->the_meta($team->ID);
+            $contact_info_metabox->the_field('_team_phone');
+            if($contact_info_metabox->get_the_value() != ''){ 
+                $team_contact_info .= '<li class="phone"><i class="fa fa-phone"></i>'.msd_str_fmt($contact_info_metabox->get_the_value(),'phone').'</li>';
+            } 
+            
+            $contact_info_metabox->the_field('_team_mobile');
+            if($contact_info_metabox->get_the_value() != ''){
+                $team_contact_info .= '<li class="mobile"><i class="fa fa-mobile-phone"></i> '.msd_str_fmt($contact_info_metabox->get_the_value(),'phone').'</li>';
+            }
+            
+            $contact_info_metabox->the_field('_team_linked_in');
+            if($contact_info_metabox->get_the_value() != ''){
+                $team_contact_info .= '<li class="linkedin"><a href="'.$contact_info_metabox->get_the_value().'"><i class="fa fa-linkedin-square"></i> Connect</a></li>';
+            }
+            
+            $contact_info_metabox->the_field('_team_bio_sheet');
+            if($contact_info_metabox->get_the_value() != ''){
+                $team_contact_info .= '<li class="vcard"><a href="'.$contact_info_metabox->get_the_value().'"><i class="fa fa-download-alt"></i> Download Bio</a></li>';
+            }
+            
+            $contact_info_metabox->the_field('_team_email');
+            if($contact_info_metabox->get_the_value() != ''){
+                $team_contact_info .= '<li class="email"><i class="fa fa-envelope"></i> '.msd_str_fmt($contact_info_metabox->get_the_value(),'email').'</li>';
+            }
+            $teamstr = '
+            <section class="team-member '.$practice_areas.' '.$team->post_name.'" href="'.get_post_permalink($team->ID).'">
+                <div class="headshot">
+                    '.$headshot.'
+                </div>
+                <div class="info">
+                    <h4>'.$team->post_title.'</h4>
+                    <h5>'.$jobtitle_metabox->get_the_value('jobtitle').'</h5>
+                    ';
+            $teamstr .= '
+                </div>
+                <ul class="contact-info">
+                    '.$team_contact_info.'
+                </ul>
+                <div class="bio">
+                    '.$mini_bio.'
+                </div>
+            </section>';
+            return $teamstr;
+    }   
+        
+        function sort_by_lastname( $a, $b ) {
+            return $a->lastname == $b->lastname ? 0 : ( $a->lastname < $b->lastname ) ? -1 : 1;
+        } 
+        
+        function get_all_practice_areas(){
+            return get_terms('practice_area',array('orderby'=>'slug','order'=>'ASC'));
+        }
+        
+        function msd_add_team_member_headshot(){
+            global $post;
+            //setup thumbnail image args to be used with genesis_get_image();
+            $size = 'headshot-lg'; // Change this to whatever add_image_size you want
+            $default_attr = array(
+                    'class' => "attachment-$size $size",
+                    'alt'   => $post->post_title,
+                    'title' => $post->post_title,
+            );
+        
+            // This is the most important part!  Checks to see if the post has a Post Thumbnail assigned to it. You can delete the if conditional if you want and assume that there will always be a thumbnail
+            genesis_markup( array(
+                'html5'   => '<section %s>',
+                'xhtml'   => '<div id="headshot" class="headshot-wrapper">',
+                'context' => 'headshot'
+            ) );
+            do_action('msdlab_before_team_member_headshot');
+            if ( has_post_thumbnail() ) {
+                printf( '%s', genesis_get_image( array( 'size' => $size, 'attr' => $default_attr ) ) );
+            }
+            do_action('msdlab_after_team_member_headshot');
+            genesis_markup( array(
+                'html5'   => '</section>',
+                'xhtml'   => '</div>'
+            ) );
+        }
+        /**
+         * Callback for dynamic Genesis 'genesis_attr_$context' filter.
+         * 
+         * Add custom attributes for the custom filter.
+         * 
+         * @param array $attributes The element attributes
+         * @return array $attributes The element attributes
+         */
+        function msdlab_headshot_context_filter( $attributes ){
+                $attributes['class'] .= ' alignleft';
+                // return the attributes
+                return $attributes;
+        }
+        
+        function msd_team_member_contact_info(){
+            global $post,$contact_info_metabox;
+            $fields = array(
+                    'phone' => 'phone',
+                    'mobile' => 'mobile-phone',
+                    'linkedin' => 'linkedin-square',
+                    'vcard' => 'download-alt',
+                    'email' => 'envelope-alt',
+            );
+            ?>
+            <ul class="team-member-contact-info">
+                <?php $contact_info_metabox->the_field('_team_phone'); ?>
+                <?php if($contact_info_metabox->get_the_value() != ''){ ?>
+                    <li class="phone"><i class="fa fa-phone"></i> <?php print msd_str_fmt($contact_info_metabox->get_the_value(),'phone'); ?></li>
+                <?php } ?>
+                
+                <?php $contact_info_metabox->the_field('_team_mobile'); ?>
+                <?php if($contact_info_metabox->get_the_value() != ''){ ?>
+                    <li class="mobile"><i class="fa fa-mobile-phone"></i> <?php print msd_str_fmt($contact_info_metabox->get_the_value(),'phone'); ?></li>
+                <?php } ?>
+                
+                <?php $contact_info_metabox->the_field('_team_linked_in'); ?>
+                <?php if($contact_info_metabox->get_the_value() != ''){ ?>
+                    <li class="linkedin"><a href="<?php print $contact_info_metabox->get_the_value(); ?>"><i class="fa fa-linkedin-square"></i></a></li>
+                <?php } ?>
+                
+                <?php $contact_info_metabox->the_field('_team_bio_sheet'); ?>
+                <?php if($contact_info_metabox->get_the_value() != ''){ ?>
+                    <li class="vcard"><a href="<?php print $contact_info_metabox->get_the_value(); ?>"><i class="fa fa-download-alt"></i></a></li>
+                <?php } ?>
+                
+                <?php $contact_info_metabox->the_field('_team_email'); ?>
+                <?php if($contact_info_metabox->get_the_value() != ''){ ?>
+                    <li class="email"><a href="mailto:<?php print antispambot($contact_info_metabox->get_the_value());?>" class="email"><i class="fa fa-envelope"></i> <?php //print msd_str_fmt($contact_info_metabox->get_the_value(),'email'); ?></a></li>
+                <?php } ?>
+            </ul>
+            <?php
+        }
+        function msd_team_additional_info(){
+            global $post,$additional_info;
+            $fields = array(
+                    'experience' => 'Experience',
+                    'decisions' => 'Notable Decisions',
+                    'honors' => 'Honors/Distinctions',
+                    'admissions' => 'Admissions',
+                    'affiliations' => 'Professional Affiliations',
+                    'community' => 'Community Involvement',
+                    'presentations' => 'Presentations',
+                    'publications' => 'Publications',
+                    'education' => 'Education',
+            );
+            $i = 0; ?>
+            <h3 class="toggle">More Info<span class="expand">Expand <i class="fa fa-angle-down"></i><span class="collapse">Collapse <i class="fa fa-angle-up"></i></h3>
+            <ul class="team-additional-info">
+            <?php
+            foreach($fields AS $k=>$v){
+            ?>
+                <?php $additional_info->the_field('_team_'.$k); ?>
+                <?php if($additional_info->get_the_value() != ''){ ?>
+                    <li>
+                        <h3><?php print $v; ?></h3>
+                        <?php print font_awesome_lists(apply_filters('the_content',$additional_info->get_the_value())); ?>
+                    </li>
+                <?php 
+                $i++;
+                }
+            } ?>
+            </ul>
+            <?php
+        }
+        function font_awesome_lists($str){
+            $str = strip_tags($str,'<a><li><ul><h3><b><strong><i>');
+            $str = preg_replace('/<ul(.*?)>/i','<ul class="icons-ul"\1>',$str);
+            $str = preg_replace('/<li>/i','<li><i class="fa fa-li fa fa-caret-right"></i>',$str);
+            return $str;
+        }
+        function msd_team_sidebar(){
+            global $post,$primary_practice_area;
+            $terms = wp_get_post_terms($post->ID,'practice_area');
+            $ppa = $primary_practice_area->get_the_value('primary_practice_area');
+            print '<div class="sidebar-content">';
+            if(count($terms)>0){
+                print '<div class="widget">
+                    <div class="widget-wrap">
+                    <h4 class="widget-title widgettitle">Practice Areas</h4>
+                    <ul>';
+                foreach($terms AS $term){
+                    if($term->slug == $ppa){
+                        if($test = get_page_by_path('/practice-areas/'.$term->slug)){
+                         $ret = '<li><a href="/practice-areas/'.$term->slug.'">'.$term->name.'</a></li>'.$ret;
+                        } else {
+                         $ret = '<li>'.$term->name.'</li>'.$ret;
+                        }
+                    } else {
+                        if($test = get_page_by_path('/practice-areas/'.$term->slug)){
+                         $ret .= '<li><a href="/practice-areas/'.$term->slug.'">'.$term->name.'</a></li>';
+                        } else {
+                         $ret .= '<li>'.$term->name.'</li>';
+                        }
+                    }
+                }
+                print $ret;
+                print '</ul>
+                </div>
+                </div>';
+            }
+            dynamic_sidebar('team-sidebar');
+            print '</div>';
+        }
+        function msd_do_team_member_job_title() {
+            global $jobtitle_metabox;
+            $jobtitle_metabox->the_meta();
+            $jobtitle = $jobtitle_metabox->get_the_value('jobtitle');
+        
+            if ( strlen( $jobtitle ) == 0 )
+                return;
+        
+            $jobtitle = sprintf( '<h2 class="entry-subtitle">%s</h2>', apply_filters( 'genesis_post_title_text', $jobtitle ) );
+            echo apply_filters( 'genesis_post_title_output', $jobtitle ) . "\n";
+        }
+        function team_members_shortcode_handler($atts){
+            extract( shortcode_atts( array(
+                'random' => false,
+                'count' => 1,
+                'practice_area' => false,
+                'full' => false
+            ), $atts ) );
+            if($practice_area){
+                $team = $this->get_team_member_by_practice($practice_area);
+            } else {
+                $team = $this->get_all_team_members();
+            }
+            $class = $full?' full':'';
+            $ret = '<div id="team-members" class="team-members'.$class.'">';
+            foreach($team AS $team_member){
+                if($full){
+                    $ret .= $this->team_display_full($team_member);
+                } else {
+                    $ret .= $this->team_display($team_member);
+                }
+            }
+            $ret .= '</div>';
+            return $ret;
+        }
+  } //End Class
+} //End if class exists statement
